b094a4c7c8465edb6947dca6023994677c6d5929
Jack <lusi@seadaka.com>
2022-06-02 17:54:19 +0800

    update

---
+++ crawl_knife/captcha/recaptcha.py
@@ -0,0 +1,37 @@
+"""
+解决 recaptcha
+"""
+from selenium import webdriver
+
+
+def solve_by_2captcha(driver, api_key):
+    """
+
+    :param driver:
+    :param api_key:
+    :return:
+    """
+
+
+def solve_by_buster(driver):
+    """
+    通过 扩展插件 buster 解决
+    :param driver:
+    :return:
+    """
+
+
+def solve_by_speed_reg(driver):
+    """
+    通过 语音识别
+    :param driver:
+    :return:
+    """
+
+
+def solve_by_yolov5(driver):
+    """
+    通过 yolo 解决
+    :param driver:
+    :return:
+    """
