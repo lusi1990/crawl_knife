a9951cb55505817dedf2a98248a7c8f8273c374c
Jack <lusi@seadaka.com>
2022-06-21 17:46:49 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 
 setup(
     name='crawl_knife',
-    version='0.0.13',
+    version='0.0.14',
     description='Python crawl tool',
     author='Master',
     author_email='lusi2114@gmail.com',
     url='https://github.com/lusi1990/crawl_knife',
     install_requires=[
-        "selenium>=3.141.0,<4.0",
+        "selenium>=4.0",
         "selenium-wire>=4.6.4",
         "requests>=2.27.1",
         "webdriver-manager>=3.7.0",
