ed0f69e6b3d3c6e4154de43f2f70f18a0335e630
Jack <lusi@seadaka.com>
2022-07-25 16:49:15 +0800

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='crawl_knife',
-    version='0.1.0',
+    version='0.1.1',
     description='Python crawl tool',
     author='Master',
     author_email='lusi2114@gmail.com',
